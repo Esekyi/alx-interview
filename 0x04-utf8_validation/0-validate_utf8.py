c7f41c56279706a58a4ecd358b164f9b455a7282
[esekyi] <[sskert10@gmail.com]>
2024-11-01 05:44:52 +0000

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """UTF-8 Validation"""
-from typing import List
 
 
-def validUTF8(data: List[int]) -> bool:
+def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding
     params:
@@ -11,7 +10,7 @@ def validUTF8(data: List[int]) -> bool:
     returns: True if data is valid UTF-8, else False
     """
 
-    num_bytes: int = 0
+    num_bytes = 0
     for byte in data:
         byte = byte & 0xFF
 
